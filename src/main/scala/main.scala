e095addd21abb2c3bd29fe0c2e1b4adbdf243387
trickyPupa <timofeylabor@gmail.com>
2026-04-08 18:16:38 +0300

    csv writer

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,15 +1,15 @@
-package main
-
 import trig.Sin
+import util.CsvWriter
 
 @main
 def main(): Unit = {
-  val precision = BigDecimal.decimal(1.123d)
-  val x = BigDecimal.decimal(0.3)
-  
+  val precision = BigDecimal.decimal(0.123)
   val sin = Sin()
   
-  val res = sin.calculate(x, null)
-  
-  println(res)
+  CsvWriter.write(sin, 
+    BigDecimal(0), 
+    BigDecimal(5), 
+    BigDecimal(0.2), 
+    precision, 
+    "sin.csv")
 }
--- src/main/scala/util/CsvWriter.scala
+++ src/main/scala/util/CsvWriter.scala
@@ -1,7 +1,41 @@
 package util
 
-import java.io.BufferedWriter
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
+import scala.util.Using
+import scala.util.control.NonFatal
 
-class CsvWriter {
+object CsvWriter {
+  def write(function: func.Function,
+            start: BigDecimal,
+            end: BigDecimal,
+            step: BigDecimal,
+            precision: BigDecimal,
+            fileName: String): Unit = {
+    require(step > 0, "Шаг должен быть >0")
+    require(precision > 0 && precision < 1, "Точность должна быть >0 и <1")
+    require(start <= end, "Начальная точка должна быть <= конечной")
 
+    val path = Paths.get(fileName)
+
+    Using.resource(Files.newBufferedWriter(path, StandardCharsets.UTF_8)) { writer =>
+      writer.write("x,result\n")
+
+      var x = start
+      while (x <= end) {
+        val result =
+          try formatDecimal(function.calculate(x, precision), 4)
+          catch {
+            case NonFatal(_) => "undefined"
+          }
+
+        writer.write(s"$x,${result.format()}\n")
+        x += step
+      }
+    }
+  }
+
+  private def formatDecimal(value: BigDecimal, digits: Int): String = {
+    value.setScale(digits, BigDecimal.RoundingMode.HALF_EVEN).toString
+  }
 }
